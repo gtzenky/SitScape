553543f7bb69c9be9f52946b9b75a9d90c86b438
Thong Nguyen <thongmgnguyen@kms-technology.com>
2015-04-20 16:57:09 +0700

    log

--- src/main/java/com/sitscape/controller/HomeController.java
+++ src/main/java/com/sitscape/controller/HomeController.java
@@ -1,13 +1,18 @@
 package com.sitscape.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
   
+  private final Log logger = LogFactory.getLog(getClass());
+  
   @RequestMapping("/")
   public String Main() {
+    logger.info("main class");
     return "Hello world!";
   }
 }

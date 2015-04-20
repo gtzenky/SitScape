d91709bc862f778f821604f6ce64a69493a3eef7
Thong Nguyen <thongmgnguyen@kms-technology.com>
2015-04-20 15:36:25 +0700

    init code

---
+++ src/main/java/com/sitscape/Application.java
@@ -0,0 +1,13 @@
+package com.sitscape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+
+@SpringApplicationConfiguration
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/sitscape/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.sitscape.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+  
+  @RequestMapping("/")
+  public String Main() {
+    return "Hello world!";
+  }
+}

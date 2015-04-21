1163ebfecd643cd4605afa9919925475fee8de1b
Thong Nguyen <thongmgnguyen@kms-technology.com>
2015-04-21 10:19:29 +0700

    add boot strap

--- src/main/java/com/sitscape/config/WebConfig.java
+++ src/main/java/com/sitscape/config/WebConfig.java
@@ -40,7 +40,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 	public InternalResourceViewResolver viewResolver() {
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
 		viewResolver.setPrefix("/WEB-INF/views/");
-		viewResolver.setSuffix(".jsp");
+		viewResolver.setSuffix(".html");
 		return viewResolver;
 	}
 
